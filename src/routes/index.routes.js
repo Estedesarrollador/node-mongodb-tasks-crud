625912a1940d3a921e7dfd913daf6723673d3343
DavidRichard <dquispe@prestamype.com>
2022-03-18 09:35:36 -0500

    repared errors

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,19 +1,26 @@
 import { Router } from "express";
-import * as taskCtrl from "../controllers/tasks.controller";
+import {
+  createTask,
+  deleteTask,
+  renderTasks,
+  taskToggleDone,
+  renderTaskEdit,
+  editTask,
+} from "../controllers/tasks.controllers";
 
 const router = Router();
 
-router.get("/", taskCtrl.renderTasks)
+router.get("/", renderTasks)
 
-router.post("/tasks/add", taskCtrl.createTask);
+router.post("/tasks/add", createTask);
 
-router.get("/tasks/:id/edit", taskCtrl.renderTaskEdit);
+router.get("/tasks/:id/edit", renderTaskEdit);
 
-router.post("/tasks/:id/edit", taskCtrl.editTask);
+router.post("/tasks/:id/edit", editTask);
 
-router.get("/tasks/:id/delete", taskCtrl.deleteTask);
+router.get("/tasks/:id/delete", deleteTask);
 
-router.get("/tasks/:id/toggleDone", taskCtrl.taskToggleDone);
+router.get("/tasks/:id/toggleDone", taskToggleDone);
 
 
 
