6fcd20068d92634f8cbeac4ebb0f0f13d20963bc
DavidRichard <dquispe@prestamype.com>
2022-03-18 17:36:30 -0500

    make it better

--- src/database.js
+++ src/database.js
@@ -3,8 +3,7 @@ import { MONGODB_URI } from "./config";
 
 (async () => {
   try {
-    // const db = await connect(MONGODB_URI);
-    const db = await connect('mongodb+srv://basededatos:rootroot@cluster0.ie3jl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+    const db = await connect(MONGODB_URI);
     console.log("DB connected to:", db.connection.name);
   } catch (error) {
     console.error(error);
