38101d57841b696ba72d39e211eaaec2b72808e9
DavidRichard <dquispe@prestamype.com>
2022-03-20 11:37:27 -0500

    ok

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,18 +1,18 @@
 import { Router } from "express";
-import {renderTasks} from "../controllers/tasks.controller";
+import * as taskCtrl from "../controllers/tasks.controller";
 
 const router = Router();
 
-router.get("/", renderTasks);
+router.get("/", taskCtrl.renderTasks);
 
-// router.post("/tasks/add", taskCtrl.createTask);
+router.post("/tasks/add", taskCtrl.createTask);
 
-// router.get("/tasks/:id/edit", taskCtrl.renderTaskEdit);
+router.get("/tasks/:id/edit", taskCtrl.renderTaskEdit);
 
-// router.post("/tasks/:id/edit", taskCtrl.editTask);
+router.post("/tasks/:id/edit", taskCtrl.editTask);
 
-// router.get("/tasks/:id/delete", taskCtrl.deleteTask);
+router.get("/tasks/:id/delete", taskCtrl.deleteTask);
 
-// router.get("/tasks/:id/toggleDone", taskCtrl.taskToggleDone);
+router.get("/tasks/:id/toggleDone", taskCtrl.taskToggleDone);
 
 export default router;
