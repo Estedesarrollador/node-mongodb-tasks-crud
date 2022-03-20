65c6f1a88aeee6b812a85dd6fbafd8698100bbce
DavidRichard <dquispe@prestamype.com>
2022-03-20 12:14:09 -0500

    up

--- src/app.js
+++ src/app.js
@@ -2,34 +2,28 @@ import path from "path";
 import express from "express";
 import morgan from "morgan";
 import { create } from "express-handlebars";
-
 import indexRoutes from "./routes/index.routes";
 
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
-
 app.set("views", path.join(__dirname, "views"));
-
 const exphbs2 = create({
   extname: ".hbs",
   layoutsDir: path.join(app.get("views"), "layouts"),
   partialsDir: path.join(app.get("views"), "partials"),
   defaultLayout: "main",
 });
-
 app.engine(".hbs", exphbs2.engine);
 app.set("view engine", ".hbs");
 
 // Middelewares
 app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.use(indexRoutes);
-
-app.use(express.urlencoded({ extended: false }));
-
 
 // static files
 app.use(express.static(path.join(__dirname, "public")));
